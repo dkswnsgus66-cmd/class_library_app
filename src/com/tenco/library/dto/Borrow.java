85bd8bcb638d21b1a8b3e0666b6534fd1b876d42
unknown <dkswnsgus66@gmail.com>
2026-04-06 15:10:31 +0900

    feat:book,student,borrow dto 구현

--- src/com/tenco/library/dto/Borrow.java
+++ src/com/tenco/library/dto/Borrow.java
@@ -1,4 +1,22 @@
 package com.tenco.library.dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class Borrow {
+
+    private int id;
+    private int book_Id;
+    private int student_Id;
+    private LocalDate borrow_Date;
+    private LocalDate return_Date;
+
 }
